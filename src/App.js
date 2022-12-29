a8653c9a910721bd26a24856817201ae8267715b
Justin Signo <justinsigno@yahoo.com>
2022-12-29 17:03:33 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BroswerRouter as Router, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/'></Route>
+          <Route path='/cart'></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
